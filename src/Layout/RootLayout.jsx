8572439855cc7454d9c58b8aeac0967fd45b4d0f
Asif-vai <mr3366533@gmail.com>
2025-10-19 13:45:28 +0600

    use sticky in layout

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -20,7 +20,7 @@ function RootLayout() {
       </header>
       <main className="border w-11/12 mx-auto grid grid-cols-12 p-1">
         {/* left aside */}
-        <aside className="col-span-3">
+        <aside className="col-span-3 sticky top-0 h-fit">
           <LeftAside></LeftAside>
         </aside>
         <section className="col-span-6">
@@ -29,7 +29,7 @@ function RootLayout() {
           </div>
           <Outlet />
         </section>
-        <aside className="col-span-3">
+        <aside className="col-span-3 sticky top-0 h-fit">
           <RightAside></RightAside>
         </aside>
       </main>
