5985ecf0b10a4868f3d56720aa01e531b8ac89ba
Asif-vai <mr3366533@gmail.com>
2025-10-19 13:41:07 +0600

    complete leftside

--- src/components/LeftAside/LeftAside.jsx
+++ src/components/LeftAside/LeftAside.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CategoryLists from '../../pages/CategoryLists/CategoryLists'
+import Adds from '../../pages/Adds/Adds'
 
 function LeftAside() {
   return (
     <div>
       <CategoryLists/>
+      <Adds/>
     </div>
   )
 }
---
+++ src/pages/Adds/Adds.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import pic1 from '../../assets/1.png'
+import pic2 from '../../assets/2.png'
+import pic3 from '../../assets/3.png'
+function Adds() {
+  return (
+    <div>
+      <div className='mb-4 space-y-3 p-3'>
+        <img src={pic1} alt="" />
+        <p>Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+      </div>
+      <div className='mb-4 space-y-3 p-3'> 
+        <img src={pic2} alt="" />
+        <p>Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+      </div>
+      <div className='mb-4 space-y-3 p-3'>
+        <img src={pic3} alt="" />
+        <p>Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+      </div>
+    </div>
+  )
+}
+
+export default Adds
